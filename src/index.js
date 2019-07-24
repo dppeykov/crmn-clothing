68ca68752d48b2b20d5e8b1f390e67544811247b
Damyan Peykov <dppeykov@gmail.com>
2019-07-24 12:57:04 +0200

    Adding routes
    With react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
 
 import "./styles.css";
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/hats" component={HatsPage} />
     </div>
   );
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
