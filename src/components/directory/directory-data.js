dd884836eb9ed8897005c18121b8303b362ae8ba
Damyan Peykov <dppeykov@gmail.com>
2019-07-24 12:14:33 +0200

    Commiting changes

--- src/components/directory/directory-data.js
+++ src/components/directory/directory-data.js
@@ -8,26 +8,29 @@ const sections = [
   },
   {
     title: "jackets",
-    imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    imageUrl:
+      "https://www.ignitionmarketing.co.za/wp-content/uploads/2018/10/Finch-Jacket-corporate-Clothing-promotional-product-1.jpg",
     id: 2,
     linkUrl: "shop/jackets"
   },
   {
     title: "sneakers",
-    imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    imageUrl: "https://media.timeout.com/images/103532269/630/472/image.jpg",
     id: 3,
     linkUrl: "shop/sneakers"
   },
   {
     title: "womens",
-    imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXA_4UfIsApNmQQb-x-TBhUQPG4paV7Fzz8X97IAAmsZT6lW7XxQ",
     size: "large",
     id: 4,
     linkUrl: "shop/womens"
   },
   {
     title: "mens",
-    imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRr3QZAb7zfFhrw7JKUu8hGu6n9e3IFW_R1A74xF_Eav7KNT0iX",
     size: "large",
     id: 5,
     linkUrl: "shop/mens"
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, url }) => (
-  <div style={{ backgroundImage: `url(${url})` }} className="menu-item">
+const MenuItem = ({ title, url, size }) => (
+  <div
+    style={{ backgroundImage: `url(${url})` }}
+    className={`${size} menu-item`}
+  >
     <div className="content">
       <h1 className="title">{title}</h1>
       <span className="subtitle">SHOP NOW</span>
