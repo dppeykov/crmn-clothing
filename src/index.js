4bab897feb3869ae7dc2a5be5909090d17337bbf
Damyan Peykov <dppeykov@gmail.com>
2019-07-24 15:34:37 +0200

    Uploading updates
    End of video 68 in section 6

--- src/index.js
+++ src/index.js
@@ -4,20 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 import { Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 import "./styles.css";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
       <Route exact path="/" component={HomePage} />
-      <Route exact path="/shop/hats" component={HatsPage} />
+      <Route exact path="/shop" component={ShopPage} />
     </div>
   );
 }
