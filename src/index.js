866e917caace1ab320acea9b5b56d8d2f6bbad0d
Damyan Peykov <dppeykov@gmail.com>
2019-07-26 12:39:37 +0200

    Finishing video 70 - section 6

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+
+import Header from "./components/header/header-component";
 
 import "./styles.css";
 
 function App() {
   return (
     <div>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/shop" component={ShopPage} />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
