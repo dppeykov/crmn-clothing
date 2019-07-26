3e0765b74675e4dcd58ff1ff070dc7c5eb929fe6
Damyan Peykov <dppeykov@gmail.com>
2019-07-26 11:22:10 +0200

    Changing the pictures
    End of Section 5  - starting section 6

--- src/components/directory/directory-data.js
+++ src/components/directory/directory-data.js
@@ -1,36 +1,32 @@
 const sections = [
   {
     title: "hats",
-    imageUrl:
-      "http://static1.squarespace.com/static/51b601c0e4b0f42908f20456/56b4ff59ab48de9628931177/56b7e0ea45bf211a61d8b27c/1528679907202/?format=1500w",
+    imageUrl: "https://picsum.photos/id/63/400/600",
     id: 1,
     linkUrl: "shop/hats"
   },
   {
     title: "jackets",
-    imageUrl:
-      "https://www.ignitionmarketing.co.za/wp-content/uploads/2018/10/Finch-Jacket-corporate-Clothing-promotional-product-1.jpg",
+    imageUrl: "https://picsum.photos/id/44/400/600",
     id: 2,
     linkUrl: "shop/jackets"
   },
   {
     title: "sneakers",
-    imageUrl: "https://media.timeout.com/images/103532269/630/472/image.jpg",
+    imageUrl: "https://picsum.photos/id/55/400/600",
     id: 3,
     linkUrl: "shop/sneakers"
   },
   {
     title: "womens",
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXA_4UfIsApNmQQb-x-TBhUQPG4paV7Fzz8X97IAAmsZT6lW7XxQ",
+    imageUrl: "https://picsum.photos/id/56/500/700",
     size: "large",
     id: 4,
     linkUrl: "shop/womens"
   },
   {
     title: "mens",
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRr3QZAb7zfFhrw7JKUu8hGu6n9e3IFW_R1A74xF_Eav7KNT0iX",
+    imageUrl: "https://picsum.photos/id/57/500/700",
     size: "large",
     id: 5,
     linkUrl: "shop/mens"
