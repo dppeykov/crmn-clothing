5923bc6e7223f284d28a9beba0905453fcc47789
Damyan Peykov <dppeykov@gmail.com>
2019-07-26 12:13:34 +0200

    Ending video 69 - section 6
    Added the pics and collection-preview and collection-item components

--- src/pages/shop/shop.data.js
+++ src/pages/shop/shop.data.js
@@ -7,55 +7,55 @@ const SHOP_DATA = [
       {
         id: 1,
         name: "Brown Brim",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/1/200/300",
         price: 25
       },
       {
         id: 2,
         name: "Blue Beanie",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/2/200/300",
         price: 18
       },
       {
         id: 3,
         name: "Brown Cowboy",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/3/200/300",
         price: 35
       },
       {
         id: 4,
         name: "Grey Brim",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/4/200/300",
         price: 25
       },
       {
         id: 5,
         name: "Green Beanie",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/5/200/300",
         price: 18
       },
       {
         id: 6,
         name: "Palm Tree Cap",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/6/200/300",
         price: 14
       },
       {
         id: 7,
         name: "Red Beanie",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/7/200/300",
         price: 18
       },
       {
         id: 8,
         name: "Wolf Cap",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/8/200/300",
         price: 14
       },
       {
         id: 9,
         name: "Blue Snapback",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/9/200/300",
         price: 16
       }
     ]
@@ -68,49 +68,49 @@ const SHOP_DATA = [
       {
         id: 10,
         name: "Adidas NMD",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/10/200/300",
         price: 220
       },
       {
         id: 11,
         name: "Adidas Yeezy",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/11/200/300",
         price: 280
       },
       {
         id: 12,
         name: "Black Converse",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/12/200/300",
         price: 110
       },
       {
         id: 13,
         name: "Nike White AirForce",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/13/200/300",
         price: 160
       },
       {
         id: 14,
         name: "Nike Red High Tops",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/14/200/300",
         price: 160
       },
       {
         id: 15,
         name: "Nike Brown High Tops",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/15/200/300",
         price: 160
       },
       {
         id: 16,
         name: "Air Jordan Limited",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/16/200/300",
         price: 190
       },
       {
         id: 17,
         name: "Timberlands",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/17/200/300",
         price: 200
       }
     ]
@@ -123,31 +123,31 @@ const SHOP_DATA = [
       {
         id: 18,
         name: "Black Jean Shearling",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/21/200/300",
         price: 125
       },
       {
         id: 19,
         name: "Blue Jean Jacket",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/22/200/300",
         price: 90
       },
       {
         id: 20,
         name: "Grey Jean Jacket",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/23/200/300",
         price: 90
       },
       {
         id: 21,
         name: "Brown Shearling",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/24/200/300",
         price: 165
       },
       {
         id: 22,
         name: "Tan Trench",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/25/200/300",
         price: 185
       }
     ]
@@ -160,43 +160,43 @@ const SHOP_DATA = [
       {
         id: 23,
         name: "Blue Tanktop",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/31/200/300",
         price: 25
       },
       {
         id: 24,
         name: "Floral Blouse",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/32/200/300",
         price: 20
       },
       {
         id: 25,
         name: "Floral Dress",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/33/200/300",
         price: 80
       },
       {
         id: 26,
         name: "Red Dots Dress",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/34/200/300",
         price: 80
       },
       {
         id: 27,
         name: "Striped Sweater",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/35/200/300",
         price: 45
       },
       {
         id: 28,
         name: "Yellow Track Suit",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/36/200/300",
         price: 135
       },
       {
         id: 29,
         name: "White Blouse",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/37/200/300",
         price: 20
       }
     ]
@@ -209,37 +209,37 @@ const SHOP_DATA = [
       {
         id: 30,
         name: "Camo Down Vest",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/41/200/300",
         price: 325
       },
       {
         id: 31,
         name: "Floral T-shirt",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/42/200/300",
         price: 20
       },
       {
         id: 32,
         name: "Black & White Longsleeve",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/43/200/300",
         price: 25
       },
       {
         id: 33,
         name: "Pink T-shirt",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/44/200/300",
         price: 25
       },
       {
         id: 34,
         name: "Jean Long Sleeve",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/45/200/300",
         price: 40
       },
       {
         id: 35,
         name: "Burgundy T-shirt",
-        imageUrl: "https://picsum.photos/200/300",
+        imageUrl: "https://picsum.photos/id/46/200/300",
         price: 25
       }
     ]
