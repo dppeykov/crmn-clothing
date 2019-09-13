59651291bb78c7cd912cb0ecb8a2fffac5e80821
Damyan Peykov <dppeykov@gmail.com>
2019-09-13 12:50:48 +0200

    Adding the sign up functionality

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,14 +3,40 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const config = {
-  apiKey: "AIzaSyDYRsYEy556gF2P_82ejoPQfcgQ-NPc16w",
-  authDomain: "crwn-db-5ca89.firebaseapp.com",
-  databaseURL: "https://crwn-db-5ca89.firebaseio.com",
-  projectId: "crwn-db-5ca89",
-  storageBucket: "",
-  messagingSenderId: "119291360201",
-  appId: "1:119291360201:web:6ce2df1465216df8"
+    apiKey: "AIzaSyDYRsYEy556gF2P_82ejoPQfcgQ-NPc16w",
+    authDomain: "crwn-db-5ca89.firebaseapp.com",
+    databaseURL: "https://crwn-db-5ca89.firebaseio.com",
+    projectId: "crwn-db-5ca89",
+    storageBucket: "",
+    messagingSenderId: "119291360201",
+    appId: "1:119291360201:web:6ce2df1465216df8"
 };
+
+export const createUserProfileDocument = async(userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exist) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+    return userRef;
+
+}
 
 firebase.initializeApp(config);
 
